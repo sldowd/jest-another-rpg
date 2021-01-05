8f6c91c2e2f7ece6f34636b74dddd343697fc57a
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-01-05 12:52:10 -0500

    added getStats and getInventory methods to player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,8 +4,6 @@ const Potion = require('../lib/Potion')
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
-
 test('creates a player object', () => {
     const player = new Player('Dave');
 
@@ -17,3 +15,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test('gets player stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+})
